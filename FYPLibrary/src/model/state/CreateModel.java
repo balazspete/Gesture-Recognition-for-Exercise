731dcfde5ff853e37156f37182d5e6edd859b213
Balazs Pete <bazsip@gmail.com>
2013-01-29 23:38:04 +0000

    Adding CreateModel
    CreateModel is used to create the vector&FuzzyState based model for
    gestures

---
+++ FYPLibrary/src/model/state/CreateModel.java
@@ -0,0 +1,23 @@
+package model.state;
+
+import java.util.Vector;
+
+public class CreateModel {
+
+	private Vector<FuzzyPoint> points;
+	
+	public CreateModel(Vector<FuzzyPoint> points) {
+		this.points = points;
+	}
+	
+	public void calculate() {
+		
+	}
+	
+	private double calculateUnit() {
+		
+		
+		return 0;
+	}
+	
+}
