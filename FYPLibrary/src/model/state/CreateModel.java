02b6904cc3b76d0d2900a7ee931f41df2b3e1417
Balazs Pete <bazsip@gmail.com>
2013-01-29 23:56:22 +0000

    Updating CreateModel

--- FYPLibrary/src/model/state/CreateModel.java
+++ FYPLibrary/src/model/state/CreateModel.java
@@ -5,8 +5,10 @@ import java.util.Vector;
 public class CreateModel {
 
 	private Vector<FuzzyPoint> points;
+	private double unit;
 	
-	public CreateModel(Vector<FuzzyPoint> points) {
+	public CreateModel(Vector<FuzzyPoint> points) throws InsufficentModelDataError {
+		if(points.size() < 2) throw new InsufficentModelDataError();
 		this.points = points;
 	}
 	
