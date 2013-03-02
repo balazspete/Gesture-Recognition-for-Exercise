36505535601dcbb3826611fc18b2da7aa394c696
Balázs Pete <bazsip@gmail.com>
2013-03-02 03:07:47 +0000

    Adding skeleton of NoiseSimulatorFilter

---
+++ FYPLibrary/src/filters/NoiseSimulatorFilter.java
@@ -0,0 +1,56 @@
+package filters;
+
+import coordinates.Coordinate;
+
+/**
+ * A class to artificially introduce noise into a coordinate
+ * @author Balazs Pete
+ *
+ */
+public class NoiseSimulatorFilter implements Filter {
+	
+	protected double noiseRatio;
+	protected double maxNoiseValue;
+	protected Filter other;
+	
+	/**
+	 * Create a new instance of NoiseSimulatorFilter with specified parameters
+	 * @param noiseRatio The probability of a distorting noise value to be introduced (range: [0; 1])
+	 * @param maxNoiseValue The maximum amount of noise that can be introduced into the coordinate
+	 */
+	public NoiseSimulatorFilter(double noiseRatio, double maxNoiseValue) {
+		setup(noiseRatio, maxNoiseValue);
+	}
+	
+	/**
+	 * Create a new instance of NoiseSimulatorFilter with specified parameters
+	 * @param noiseRatio The probability of a distorting noise value to be introduced (range: [0; 1])
+	 * @param maxNoiseValue The maximum amount of noise that can be introduced into the coordinate
+	 */
+	public NoiseSimulatorFilter(double noiseRatio, double maxNoiseValue, Filter other) {
+		this.other = other;
+		setup(noiseRatio, maxNoiseValue);
+	}
+	
+	/**
+	 * Method to help the initialization of the Object
+	 * @param noiseRatio The noise ratio
+	 * @param maxNoiseValue The maximum noise value
+	 */
+	private void setup(double noiseRatio, double maxNoiseValue) {
+		if(noiseRatio < 0 || 1 < noiseRatio) {
+			this.noiseRatio = 0;
+		} else {
+			this.noiseRatio = noiseRatio;
+		}
+		
+		this.maxNoiseValue = Math.abs(maxNoiseValue);
+	}
+
+	@Override
+	public Coordinate filter(Coordinate coordinate) {
+		
+		return null;
+	}
+
+}
